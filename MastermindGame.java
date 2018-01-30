66184830db9ea71215b01c456672224b3c1aa8fa
SleekPanther <macrobyte2@gmail.com>
2018-01-30 16:04:13 -0500

    Generate a hidden code

--- MastermindGame.java
+++ MastermindGame.java
@@ -1,7 +1,31 @@
+import java.util.*;
+
 public class MastermindGame {
+	private static final String[] validColors = {"r","o","y","g","b"};
+	private Map<String, Integer> stringToIntegerColor = new HashMap<String, Integer>();
+	
+	private int[] hiddenCode = new int[4];
+
+
+	public MastermindGame(){
+		for(int i=0; i<validColors.length; i++){
+			stringToIntegerColor.put(validColors[i], i);
+		}
+
+		generateHiddenCode();
+	}
+
+	//Random number to create code, allows repeated colors
+	private void generateHiddenCode(){
+		for(int i=0; i<hiddenCode.length; i++){
+			hiddenCode[i] = (int)(Math.random() * validColors.length) ;
+		}
+		System.out.println(Arrays.toString(hiddenCode));
+	}
+
 	
 	public static void main(String[] args) {
-		
+		MastermindGame game = new MastermindGame();
 	}
 
 }
