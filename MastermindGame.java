2687d0cf4ac7bacb6a9b25caf934eb4023fb6a99
SleekPanther <macrobyte2@gmail.com>
2018-01-31 21:44:04 -0500

    Correctly provide both kinds of output, & display message when over

--- MastermindGame.java
+++ MastermindGame.java
@@ -1,39 +1,35 @@
 import java.util.*;
 
-public class MastermindGame {					//d=gold	b=blue		o=orange		k=blacK
-	private static final String[] validColors = {"d","y","w","r", "g","b","o","k"};//"r","o","y","g","b", "w"};
-	private Map<String, Integer> stringToIntegerColor = new HashMap<String, Integer>();
+public class MastermindGame {
+	private static final String[] validColors = {
+		"w",	//White
+		"d",	//golD
+		"k",	//blacK
+		"b",	//Blue
+		"r",	//Red
+		"y",	//Yellow
+		"g",	//Green
+		"o",	//Orange
+	};
 	
-	private int[] hiddenCode = new int[4];
+	private String[] hiddenCode = new String[4];
 	private final int NUMBER_OF_TURNS = 10;
 	private String[][] guesses = new String[NUMBER_OF_TURNS][];
 
 
 	public MastermindGame(){
-		for(int i=0; i<validColors.length; i++){
-			stringToIntegerColor.put(validColors[i], i);
-		}
-
 		start();
 	}
 
 	//Random number to create code, allows repeated colors
 	private void generateHiddenCode(){
-//		for(int i=0; i<hiddenCode.length; i++){
-//			hiddenCode[i] = (int)(Math.random() * validColors.length) ;
-//		}
-		//Hack it to a specific color setting
-		hiddenCode = new int[]{0,1,2,3};
-		System.out.println("Hidden code: "+Arrays.toString(hiddenCode) + " = " + printCodeAsString(hiddenCode));
-	}
-
-	private String printCodeAsString(int[] code){
-		String codeString = "[";
-		for(int digit : code){
-			codeString += (validColors[digit] + "  ");
+		for(int i=0; i<hiddenCode.length; i++){
+			int randomIndexForColor = (int)(Math.random() * validColors.length);
+			hiddenCode[i] = validColors[randomIndexForColor];
 		}
-		codeString += "]";
-		return codeString;
+		//Hack it to a specific color setting
+		hiddenCode = new String[]{"w", "d", "k", "b"};
+		System.out.println("Hidden code: "+Arrays.toString(hiddenCode));
 	}
 
 	private void start(){
@@ -45,7 +41,7 @@ public class MastermindGame {					//d=gold	b=blue		o=orange		k=blacK
 
 		boolean correct = false;
 		for(int i=0; i<NUMBER_OF_TURNS && !correct; ){
-			System.out.print("Guess: ");
+			System.out.print("\nGuess: ");
 			String guess = keyboard.nextLine();
 			if(isValidGuessInput(guess)){
 				guesses[i] = convertGuessStringToArray(guess);
@@ -58,6 +54,13 @@ public class MastermindGame {					//d=gold	b=blue		o=orange		k=blacK
 				System.out.println("Invalid Input, try again");
 			}
 		}
+
+		if(correct){
+			System.out.println("You win");
+		}
+		else{
+			System.out.println("\nSorry, you ran out of turns\n" + "The correct code was " + Arrays.toString(hiddenCode));
+		}
 	}
 
 	private boolean isValidGuessInput(String guess){
@@ -66,35 +69,41 @@ public class MastermindGame {					//d=gold	b=blue		o=orange		k=blacK
 	}
 
 	private String[] convertGuessStringToArray(String guess){
-		return guess.trim().replaceAll(" ", "").split("");
+		return guess.trim().replaceAll(" ", "").toLowerCase().split("");
 	}
 
 	private boolean isCorrectCode(String[] guess){
 		int correctLocationAndColorCount = 0;
-		int correctColorCount = 0;
+		int correctColorWrongPlaceCount = 0;
+
 		ArrayList<Integer> finalizedIndices = new ArrayList<Integer>();
+		ArrayList<String> colorsAlreadySearched = new ArrayList<String>();
+
+		//Search for correct location & correct color
 		for(int i=0; i<guess.length; i++){
-			if(stringToIntegerColor.get(guess[i]) == hiddenCode[i]){
+			if(guess[i].equals(hiddenCode[i])){
 				correctLocationAndColorCount++;
 				finalizedIndices.add(i);
 			}
-			else{
-				System.out.println("Finalized="+finalizedIndices);
-				for(int j=0; j<hiddenCode.length; j++){		//
-					if(!finalizedIndices.contains(j) && stringToIntegerColor.get(guess[i]) == hiddenCode[j]){		//only check indeces that aren't already correct
-						correctColorCount++;
-						finalizedIndices.add(j);
-					}
+		}
+
+		//Search remaining spaces for correct colors in incorrect locations
+		for(int i=0; i<guess.length; i++){
+			for(int j=0; j<hiddenCode.length; j++){
+				if(!finalizedIndices.contains(j) && !colorsAlreadySearched.contains(guess[i]) && guess[i].equals(hiddenCode[j])){		//only check indices that aren't already correct & colors that haven't been searched
+					correctColorWrongPlaceCount++;
+					finalizedIndices.add(j);
+					colorsAlreadySearched.add(guess[i]);
 				}
 			}
 		}
 
-		System.out.println("Correct location & Color = "+correctLocationAndColorCount + "\tCorrect Color, wrong location = "+correctColorCount);
+		System.out.println("Correct location & Color = "+correctLocationAndColorCount + "\tCorrect Color, wrong location = "+correctColorWrongPlaceCount);
 
 		return correctLocationAndColorCount == hiddenCode.length;
 	}
 
-	
+
 	public static void main(String[] args) {
 		MastermindGame game = new MastermindGame();
 	}
